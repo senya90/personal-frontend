cfe1ac8d5d94cf99cbf9378e42bef2c5be6c2b05
KOMPUTER\semyon <mamoshin_sem@mail.ru>
2025-12-26 14:14:38 +0300

    Styles for LanguageSwitcher

---
+++ src/shared/ui/LanguageSwitcher/lib/constants.ts
@@ -0,0 +1,10 @@
+export const constantsButtons = [
+  {
+    locale: 'ru',
+    title: 'RU',
+  },
+  {
+    locale: 'en',
+    title: 'EN',
+  },
+]
--- src/shared/ui/LanguageSwitcher/ui/LanguageSwitcher.tsx
+++ src/shared/ui/LanguageSwitcher/ui/LanguageSwitcher.tsx
@@ -1,14 +1,33 @@
 'use client'
 
-import { usePathname, redirect } from '@/i18n/navigation'
+import cn from 'classnames'
+import { useLocale } from 'next-intl'
+
+import { constantsButtons } from '@/shared/ui/LanguageSwitcher/lib/constants'
+
+import { usePathname, Link } from '@/i18n/navigation'
+import { Locale } from '@/i18n/routing'
+
+import styles from './styles.module.css'
 
 export function LanguageSwitcher() {
   const pathname = usePathname()
+  const locale = useLocale() as Locale
 
   return (
-    <div style={{ display: 'flex', gap: 12 }}>
-      <a onClick={() => redirect({ locale: 'ru', href: pathname })}>RU</a>
-      <a onClick={() => redirect({ locale: 'en', href: pathname })}>EN</a>
+    <div className={styles.wrapper}>
+      {constantsButtons.map((l) => (
+        <Link
+          key={l.locale}
+          locale={l.locale}
+          href={pathname}
+          className={cn(styles.languageLink, {
+            [styles.active]: l.locale === locale,
+          })}
+        >
+          {l.title}
+        </Link>
+      ))}
     </div>
   )
 }
