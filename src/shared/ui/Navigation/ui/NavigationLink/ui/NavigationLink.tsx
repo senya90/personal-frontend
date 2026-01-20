35a416292e382ce6b8c1dfb804712d24e0c253a5
KOMPUTER\semyon <mamoshin_sem@mail.ru>
2026-01-20 11:41:29 +0300

    Fix navigation

--- src/shared/ui/Navigation/ui/NavigationLink/ui/NavigationLink.tsx
+++ src/shared/ui/Navigation/ui/NavigationLink/ui/NavigationLink.tsx
@@ -1,17 +1,17 @@
 import cn from 'classnames'
-import Link from 'next/link'
 import { LinkProps as NextLinkProps } from 'next/link'
 import { AnchorHTMLAttributes, ReactNode } from 'react'
 
 import { Typography } from '@/shared/ui/Typography'
 
+import { Link } from '@/i18n/navigation'
 import { usePathname } from '@/i18n/navigation'
 
 import styles from './styles.module.css'
 
 interface IProps
   extends
-    NextLinkProps,
+    Omit<NextLinkProps, 'href' | 'locale'>,
     Omit<
       AnchorHTMLAttributes<HTMLAnchorElement>,
       keyof NextLinkProps | 'href'
